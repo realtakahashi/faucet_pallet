dca7f09065c8a3d7a201a3f0c50358af92d36eea
shin.takahashi <realtakahashi.0924@gmail.com>
2020-12-20 21:26:36 +0900

    mod

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,41 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
-use frame_system::ensure_signed;
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get,
+	traits::Currency,
+	traits::ExistenceRequirement,
+	dispatch::DispatchResult,
+};
+
+use frame_system::{
+	self as system, ensure_none, ensure_signed,
+	offchain::{SignedPayload,SigningTypes},
+};
+use parity_scale_codec::{Decode, Encode};
+use hex_literal::hex;
+use sp_runtime::{AccountId32,
+	RuntimeDebug,
+	//generic,
+	transaction_validity::{
+		InvalidTransaction, TransactionSource, TransactionValidity,
+		ValidTransaction,
+	},
+};
+
+pub const NUM_VEC_LEN: usize = 10;
+pub const UNSIGNED_TXS_PRIORITY: u64 = 100;
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug)]
+pub struct Payload<Public> {
+	number: u64,
+	public: Public
+}
+
+impl <T: SigningTypes> SignedPayload<T> for Payload<T::Public> {
+	fn public(&self) -> T::Public {
+		self.public.clone()
+	}
+}
 
 #[cfg(test)]
 mod mock;
@@ -13,10 +46,14 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+type Balance<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
 	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	type Currency: Currency<Self::AccountId>;
+
 }
 
 // The pallet's runtime storage items.
@@ -98,6 +135,45 @@ decl_module! {
 					Ok(())
 				},
 			}
+		}
+
+		#[weight = 10000]
+		pub fn submit_number_unsigned(origin, number: Balance<T>, to: T::AccountId) -> DispatchResult {
+			let _ = ensure_none(origin)?;
+			//debug::info!("submit_number_unsigned: {}", number);
+			//Self::append_or_replace_number(number);
+			//Numbers::put(number);
+			let account32: AccountId32 = hex!["d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"].into();
+			let mut from32 = AccountId32::as_ref(&account32);
+			let from : T::AccountId = T::AccountId::decode(&mut from32).unwrap_or_default();
+			T::Currency::transfer(&from,&to,number,ExistenceRequirement::KeepAlive);
+			//Self::deposit_event(RawEvent::NewNumber(None, number));
+			Ok(())
+		}
+
+	}
+}
+
+impl<T: Trait> frame_support::unsigned::ValidateUnsigned for Module<T> {
+	type Call = Call<T>;
+
+	fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
+		let valid_tx = |provide| ValidTransaction::with_tag_prefix("faucet-pallet")
+			.priority(UNSIGNED_TXS_PRIORITY)
+			.and_provides([&provide])
+			.longevity(3)
+			.propagate(true)
+			.build();
+
+		match call {
+			Call::submit_number_unsigned(_number,_to) => valid_tx(b"submit_number_unsigned".to_vec()),
+			// Call::submit_number_unsigned_with_signed_payload(ref payload, ref signature) => {
+			// 	if !SignedPayload::<T>::verify::<T::AuthorityId>(payload, signature.clone()) {
+			// 		return InvalidTransaction::BadProof.into();
+			// 	}
+			// 	valid_tx(b"submit_number_unsigned_with_signed_payload".to_vec())
+			// },
+			_ => InvalidTransaction::Call.into(),
 		}
 	}
 }
